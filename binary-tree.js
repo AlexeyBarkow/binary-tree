5da010afeadb6bab6bb88d360f648bf6db57f05d
Alexey Barkow <imbarkowal@gmail.com>
2016-01-29 12:08:42 +0300

    Realize insert and isEmpty

--- binary-tree.js
+++ binary-tree.js
@@ -1,12 +1,37 @@
 'use strict';
-
+//require("./node.js");
 class BinaryTree {
 	constructor() {
-
+		this.root = null;
 	}
 
 	insert(data) {
-
+		if(!data){
+			return;
+		}
+		var newNode = new Node(data);
+		if(this.root == null){
+			this.root = newNode;
+		} else {
+			var current = this.root;
+			while(true){
+				if(data > current.data){
+					if(current.right){
+						current = current.right;
+					} else {
+						current.right = newNode;
+						break;
+					}
+				} else {
+					if(current.left){
+						current = current.left;
+					} else {
+						current.left = newNode;
+						break;
+					}
+				}
+			}
+		}
 	}
 
 	contains(data) {
@@ -22,6 +47,12 @@ class BinaryTree {
 	}
 
 	isEmpty() {
-
+		return this.root == null;
 	}
 }
+
+var binary = new BinaryTree();
+console.log(binary.isEmpty());
+binary.insert(2);
+binary.insert(3);
+console.log(binary);
